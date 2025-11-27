df5e412dafb5253601e5ed204efcc4e3f20d8f2b
SimakovArkadiy <simakovarkadii@mail.ru>
2025-11-27 21:52:11 +0500

    добавить запись

---
+++ MyVocabulary/MyVocabulary.Web/DataBase.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+namespace MyVocabulary.Web
+{
+    public class DataBase
+    {
+        //private string connectionString = "Data Source=localhost;Initial Catalog=vocabulary;Integrated Security=True;Encrypt=False";
+        //private string englishWord;
+       // private string russianWord;
+        //public DataBase(string englisWord, string rusianWord) 
+        //{ 
+         //   this.englishWord = englisWord;
+         //   this.russianWord = rusianWord;
+       // }
+        public static void PostToBase(string englishWord, string russianWord)
+        {
+            string connectionString = "Data Source=localhost;Initial Catalog=vocabulary;Integrated Security=True;Encrypt=False";
+        //DataBase dataBaseObject = new DataBase(englishWord, russianWord);
+        SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            string query = $"INSERT INTO Words(englishWord, russianWord) VALUES('{englishWord}','{russianWord}')";
+            SqlCommand cmd = new SqlCommand(query, sqlConnection);
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
--- MyVocabulary/MyVocabulary.Web/Program.cs
+++ MyVocabulary/MyVocabulary.Web/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Hosting.Server;
+using MyVocabulary.Web;
+using System.Diagnostics.Metrics;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
@@ -8,7 +13,12 @@ app.MapPost("entry2", (context) =>
     var form = context.Request.Form;
     string russianWord = form["russianWord"];
     string englishWord = form["englishWord"];
-     context.Response.WriteAsync($"{russianWord},{englishWord}");
+    DataBase.PostToBase(englishWord,russianWord);
+    var response2 = context.Response;
+    response2.ContentType = "text/plain; charset=utf-8";
+    response2.WriteAsync($"{russianWord},{englishWord}");
     return Task.CompletedTask;
 });
 app.Run();
+//Server = localhost; Database = master; Trusted_Connection = True;
+//MSSQLSERVER
