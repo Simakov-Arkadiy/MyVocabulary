7c4ce9a4fab992b6589935d6c8645140a75bfff9
SimakovArkadiy <simakovarkadii@mail.ru>
2026-02-11 22:14:28 +0500

    Настроить фронтэнд-приложение, настроить стиль компонента loginPanel

---
+++ MyVocabulary/MyVocabulary.FrontAnd.2/myvocabulary.frontand.2/src/components/LoginPanel.jsx
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+function LoginPanel() {
+    var LoginPanel = {
+        position: 'absolute',
+        margin: '0px',
+        padding: '0px',
+        left: '50%',
+        top: '30%',
+        transform: 'translate(-50%,-50%)',
+        height: '20%',
+        backgroundColor: 'rgba(199, 244, 250)'
+    };
+    return (<div style = {LoginPanel}> 
+             <h1>Hello METANIT.COM</h1>
+            </div>);
+}
+
+export default LoginPanel;
--- MyVocabulary/MyVocabulary.FrontAnd.2/myvocabulary.frontand.2/src/main.jsx
+++ MyVocabulary/MyVocabulary.FrontAnd.2/myvocabulary.frontand.2/src/main.jsx
@@ -1,9 +1,10 @@
 ﻿import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import LoginPanel from './components/LoginPanel'
+import LoginPanel from './components/LoginPanel.jsx'
+
 
 createRoot(document.getElementById('root')).render(
-  
-        <LoginPanel/>,
-  
+
+    <LoginPanel/>
+
 )
