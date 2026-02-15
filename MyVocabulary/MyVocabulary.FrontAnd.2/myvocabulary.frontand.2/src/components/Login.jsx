02d699f19cb5a9702ed0e9259809f30ea301d291
SimakovArkadiy <simakovarkadii@mail.ru>
2026-02-15 15:24:46 +0500

    добавить поле для повтора пароля

--- MyVocabulary/MyVocabulary.FrontAnd.2/myvocabulary.frontand.2/src/components/Login.jsx
+++ MyVocabulary/MyVocabulary.FrontAnd.2/myvocabulary.frontand.2/src/components/Login.jsx
@@ -4,18 +4,27 @@ import { useState } from 'react'
 
 function Login() {
 
+    const [loginInputs, setloginInput] = useState([
+        {ID: 1, text: 'логин', type: 'text', id: 'login' },
+        {ID: 2, text: 'пароль', type: 'password', id: 'password' }])
 
-
-    const registration = () => {
-        console.log('регистрация!')
+    const registration = (e) => {
+        e.preventDefault
+        const newInput = {
+            ID: Date.now(),
+            text: 'повторить пароль',
+            type: 'password',
+            id: 'password'
+        }
+        setloginInput([...loginInputs, newInput])
     }
+    
     return (
         <div>
         <form name="loginForm" method="get" action="/authorization">
         <fieldset>
                     <legend>Авторизация</legend>
-                    <LoginInput  text="логин" type="text" id="login" />
-                    <LoginInput  text="пароль" type="password" id="password"/>
+                    {loginInputs.map((loginInput) => (<LoginInput text={loginInput.text} type={loginInput.type} id={loginInput.id} key={loginInput.ID} />))}
         </fieldset>
         <p><input type="submit" value="авторизоваться"/></p>
             </form>
