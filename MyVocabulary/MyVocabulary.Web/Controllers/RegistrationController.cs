a45c65e2ea3001bb82aee886ba91a9ce70303a4c
SimakovArkadiy <simakovarkadii@mail.ru>
2026-02-23 22:47:25 +0500

    добавить контроллер регистрации

---
+++ MyVocabulary/MyVocabulary.Web/Controllers/RegistrationController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyVokabulary.Repository.Data;
+using MyVokabulary.Repository.Models;
+
+namespace MyVocabulary.Web.Controllers
+{
+    [Controller]
+    public class RegistrationController:Microsoft.AspNetCore.Mvc.Controller
+    {
+        private readonly MyVocabularyContext _context;
+
+        public RegistrationController(MyVocabularyContext context)
+        {
+            _context = context;
+        }
+
+        public string Registration(User user)
+        {
+            if (user != null)
+            {
+                _context.Users.Add(user);
+                _context.SaveChanges();
+                return "Регистрация прошла успешно";
+            }
+            else
+            {
+                return "Регистрация н прошла, допущенна ошибка";
+            }
+        }
+    }
+}
