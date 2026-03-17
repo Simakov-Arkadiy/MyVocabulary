0890014af56fcf63b49ccc75325eb43b1d6f90c3
SimakovArkadiy <simakovarkadii@mail.ru>
2026-03-17 22:36:55 +0500

    очистить проект от кода старой версии(старая версия сохранена отдельной репой)

---
+++ MyVocabulary/MyVocabulary.Web/ClassMrrr.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+using System.Collections;
+
+namespace MyVocabulary.Web
+{
+    public class ClassMrrr : ICollection
+    {
+        public int Count => throw new NotImplementedException();
+
+        public bool IsSynchronized => throw new NotImplementedException();
+
+        public object SyncRoot => throw new NotImplementedException();
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MyVocabulary/MyVocabulary.Web/Program.cs
+++ MyVocabulary/MyVocabulary.Web/Program.cs
@@ -7,117 +7,60 @@ using MyVocabulary.Web;
 using MyVokabulary.Repository.Data;
 using System;
 using System.Diagnostics.Metrics;
+using Microsoft.AspNetCore.Identity;
 
 
 
 
-//using (var a = new MyVocabularyContext())
-//{
-//    bool deleted = a.Database.EnsureDeleted();
-//    Console.WriteLine($"Database deleted:{deleted}");
-
-//    bool created = a.Database.EnsureCreated();
-//}
-
-//var serviceCollection = new ServiceCollection();
 
 
-DatabaseCreator.CreateDataBase();
+
     
     
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connection = builder.Configuration.GetConnectionString("DefaultConnection");
 
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<MyVocabularyContext>(options =>
                 options.UseSqlServer(connection));
-
 builder.Services.AddControllers();
-
+builder.Services.AddHttpLogging(options => options.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.RequestProperties);
+builder.Logging.AddFilter("Microsoft.AspNetCore.HttpLogging", LogLevel.Information);
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 
-//var myAllowSpecificOrigins = "_myAllowSpecificOrigins";
-
 builder.Services.AddCors();
-
 //builder.Host.ConfigureLogging(logging =>
 //{
 //    logging.ClearProviders();
 //    logging.AddConsole();
 //})
-
-
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
-
-
-
 var app = builder.Build();
-
-app.MapControllerRoute(
-    name: "registration",
-    pattern: "{controller=Home}/{action=Index}");
-
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/error");
+}
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpLogging();
+}
 app.UseStaticFiles();
-app.UseRouting();
+app.MapControllerRoute(name: "registration",pattern: "{controller=Home}/{action=Index}");
+app.UseHttpsRedirection();
 app.MapControllers();
-
 app.UseSwagger();
 app.UseSwaggerUI();
-
 app.UseDefaultFiles();
 app.UseRouting();
-
 app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyHeader()
-                            .AllowAnyMethod());
+                           .AllowAnyMethod());
 
-app.MapPost("entry2", (context) =>
-{
-    var form = context.Request.Form;
-    string russianWord = form["russianWord"]!;
-    string englishWord = form["englishWord"]!;
 
-    DataBase.PostToBase(englishWord,russianWord);
-    var response2 = context.Response;
-    response2.ContentType = "text/plain; charset=utf-8";
-    response2.WriteAsync("������ ���������");
-    return Task.CompletedTask;
-});
-app.MapGet("search", (context) =>
-{
-    var query = context.Request.Query;
-    string wordOfSearch = query["wordOfSearch"]!;
-    var dataBaseString = DataBase.GetFromBase(wordOfSearch);
-    var response2 = context.Response;
-    response2.ContentType = "text/plain; charset=utf-8";
-    response2.WriteAsync($"{dataBaseString.RussianWord},{dataBaseString.EnglishWord}");
-    return Task.CompletedTask;
-});
-app.MapPost("examination", (context) =>
-{
-    var form = context.Request.Form;
-    string selectedValue = form["word"]!;
-    string translationGiwenWord = form["translationGiwenWord"]!;
-    var response2 = context.Response;
-    if (selectedValue == translationGiwenWord)
-    {
-        response2.ContentType = "text/html; charset=utf-8";
-        response2.WriteAsync("<meta http-equiv=\"refresh\" content=\"2; url=/Exercise\"><h2>�����!</h2>");
-    }
-    else 
-    {
-        response2.ContentType = "text/html; charset=utf-8";
-        response2.WriteAsync("<meta http-equiv=\"refresh\" content=\"2; url=/Exercise\"><h2>�� �����!</h2>");
-    }
-    return Task.CompletedTask;
-
-});
 
 
 
